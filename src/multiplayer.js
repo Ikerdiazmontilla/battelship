87b123b863d23cc314790c68483c576fccacc8cd
Ikerdiazmontilla <ikercorrevuela@gmail.com>
2024-04-01 14:11:19 -0500

    Start refactoring multiplayer code

--- src/multiplayer.js
+++ src/multiplayer.js
@@ -1,45 +1,10 @@
-import game from './game';
+import multiplayerEventHandler from './multiplayerEventHandler';
+import EventHandler from './eventHandler';
 
 const multiplayer = {
   direction: 'horizontal',
   playerPlacing: 1,
-  populateGrid(grid, start = false) {
-    let gridContainer = document.querySelector('#grid1');
-    if (start === true) {
-      gridContainer = document.querySelector('#grid-place');
-    }
-    gridContainer.innerHTML = '';
-    grid.forEach(line => {
-      const div = document.createElement('div');
-      div.className = 'line';
-      line.forEach(slot => {
-        const square = document.createElement('div');
-        if (slot === null) {
-          square.className = 'square empty';
-        } else if (Array.isArray(slot)) {
-          const sunk = slot[1].isSunk();
-          if (sunk === false) {
-            square.className = 'square hit';
-          } else if (sunk === true) {
-            square.className = 'square sunk';
-          }
-          square.textContent = '✘';
-        } else if (slot !== null && typeof slot === 'object') {
-          square.className = 'square ship';
-        } else if (slot === 'water') {
-          square.className = 'square water';
-          square.textContent = '✘';
-        }
-        div.appendChild(square);
-      });
-      gridContainer.appendChild(div);
-    });
-  },
-  populateEnemyGrid(player1, player2) {
-    this.player1 = player1;
-    this.player2 = player2;
-    const gridContainer = document.querySelector('#grid2');
-    const grid = this.player2.getGrid();
+  showGrid(gridContainer, grid, playerNumber) {
     gridContainer.innerHTML = '';
     grid.forEach(line => {
       const div = document.createElement('div');
@@ -54,36 +19,39 @@ const multiplayer = {
             square.className = 'square sunk';
           }
           square.textContent = '✘';
-        } else if (slot === null || (slot !== null && typeof slot === 'object')) {
-          square.className = 'square empty';
-          square.addEventListener('click', multiplayer.squareClicked);
         } else if (slot === 'water') {
           square.className = 'square water';
           square.textContent = '✘';
+        } else if (playerNumber === 1) {
+          if (slot === null) {
+            square.className = 'square empty';
+          } else if (slot !== null && typeof slot === 'object') {
+            square.className = 'square ship';
+          }
+        } else if (playerNumber === 2) {
+          if (slot === null || (slot !== null && typeof slot === 'object')) {
+            square.className = 'square empty';
+            square.addEventListener('click', multiplayerEventHandler.onSquareClicked);
+          }
         }
         div.appendChild(square);
       });
       gridContainer.appendChild(div);
     });
   },
-  squareClicked: async event => {
-    const enemyGrid = document.querySelector('#grid2');
-    const column = event.currentTarget.parentNode;
-    const clickedSquare = event.currentTarget;
-    const indexY = Array.prototype.indexOf.call(column.children, clickedSquare);
-    const indexX = Array.prototype.indexOf.call(enemyGrid.children, column);
-    const hit = multiplayer.player1.attack(multiplayer.player2, indexX, indexY);
-    if (multiplayer.player2.allSunk() !== false) return multiplayer.playerWon(multiplayer.player1);
-    multiplayer.populateEnemyGrid(multiplayer.player1, multiplayer.player2);
-    multiplayer.updateBoatsAlive();
-    if (hit === false) {
-      multiplayer.showWaterDialog();
-      multiplayer.populateGrid(multiplayer.player2.getGrid());
-      multiplayer.populateEnemyGrid(multiplayer.player2, multiplayer.player1);
-      multiplayer.updateBoatsAlive();
-      multiplayer.toggleTurn();
+  populateGrid(grid, start = false) {
+    let gridContainer = document.querySelector('#grid1');
+    if (start === true) {
+      gridContainer = document.querySelector('#grid-place');
     }
-    return false;
+    multiplayer.showGrid(gridContainer, grid, 1);
+  },
+  populateEnemyGrid(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2;
+    const gridContainer = document.querySelector('#grid2');
+    const grid = this.player2.getGrid();
+    multiplayer.showGrid(gridContainer, grid, 2);
   },
   showWaterDialog() {
     const waterDialog = document.querySelector('.water-dialog');
@@ -94,23 +62,6 @@ const multiplayer = {
       waterDialog.close();
       waterDialog.classList.remove('show');
     });
-  },
-  playerWon(player) {
-    const dialogWin = document.querySelector('.win');
-    dialogWin.showModal();
-    const restart = dialogWin.querySelector('.restart');
-    restart.addEventListener('click', () => game.chooseGame());
-    restart.addEventListener('click', () => {
-      dialogWin.close();
-    });
-    const turns = document.querySelector('.turns');
-    turns.textContent = `Player ${player.number} won!!!`;
-    const winner = document.querySelector('.winner');
-    winner.textContent = `Player ${player.number} won!!!`;
-    winner.className = 'winner';
-    if (player.number === 2) {
-      winner.classList.add('two');
-    }
   },
   toggleTurn() {
     const divTurns = document.querySelector('.turns');
@@ -246,8 +197,7 @@ const multiplayer = {
   },
   addEventListeners(player1, player2) {
     const oldStartButton = document.querySelector('.place-ships .start');
-    const startButton = oldStartButton.cloneNode(true);
-    oldStartButton.parentNode.replaceChild(startButton, oldStartButton);
+    const startButton = EventHandler.removeEventListeners(oldStartButton);
     startButton.addEventListener('click', () => {
       const dialogPlace = document.querySelector('.place-ships');
       const boats = document.querySelectorAll('.boat');
@@ -270,8 +220,7 @@ const multiplayer = {
     });
 
     const oldChDirection = document.querySelector('#direction');
-    const chDirection = oldChDirection.cloneNode(true);
-    oldChDirection.parentNode.replaceChild(chDirection, oldChDirection);
+    const chDirection = EventHandler.removeEventListeners(oldChDirection);
     chDirection.addEventListener('click', () => {
       const boatDrag = document.querySelector('.boats-drag');
       const boats = document.querySelectorAll('.boat');
@@ -291,8 +240,7 @@ const multiplayer = {
     });
 
     const oldRandom = document.querySelector('.random');
-    const random = oldRandom.cloneNode(true);
-    oldRandom.parentNode.replaceChild(random, oldRandom);
+    const random = EventHandler.removeEventListeners(oldRandom);
     random.addEventListener('click', () => {
       player1.emptyGrid();
       player1.placeShipRandom(5);
